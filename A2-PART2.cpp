dd9e492ba106c931581a8ad7c3e688894d811fd5
Joodi <joodialasaad@cmail.carleton.ca>
2025-11-06 23:55:06 -0500

    added wait() for parent to wait for child completion

--- A2-PART2.cpp
+++ A2-PART2.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include <unistd.h>  //to get pid 
+#include <unistd.h>   
 #include <sys/wait.h> 
-
 
 int main() {
     pid_t pid = fork(); 
@@ -13,18 +12,19 @@ int main() {
     else if (pid == 0) {
         for (int i = 0; i < 5; ++i) {
             std::cout << "Child process: iteration " << i 
-                    << ", PID = " << getpid() << std::endl;
+                      << ", PID = " << getpid() << std::endl;
             sleep(1);
         }
+        std::cout << "Child process completed." << std::endl;
     } else {
         for (int i = 0; i < 5; ++i) {
             std::cout << "Parent process: iteration " << i 
-                    << ", PID = " << getpid() << std::endl;
+                      << ", PID = " << getpid() << std::endl;
             sleep(1);
         }
+        wait(nullptr); 
+        std::cout << "Parent: child has finished." << std::endl;
     }
 
-
-    return 0; 
-
+    return 0;
 }
