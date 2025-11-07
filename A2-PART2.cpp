b947910c3ea3e167c49a8d3f42e408e64586e068
Joodi <joodialasaad@cmail.carleton.ca>
2025-11-06 23:00:20 -0500

    added child process output showing its PID and parent PID

--- A2-PART2.cpp
+++ A2-PART2.cpp
@@ -3,9 +3,13 @@
 
 int main() {
     pid_t pid = fork(); 
-    
+
     if (pid < 0) {
         std::cerr << "Fork failed!" << std::endl;
         return 1;
     }
+    else if (pid == 0){
+        std::cout << "Child process: PID = " << getpid() << ", Parent PID = " << getppid() << std::endl;
+    }
+    
 }
