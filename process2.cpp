d7c863fc4fea098f6c7ee61aa519063983c577f0
Joodi <joodialasaad@cmail.carleton.ca>
2025-11-07 18:28:23 -0500

    implemented wait() synchronization and child termination at counter < -500

--- process2.cpp
+++ process2.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <unistd.h>
 
-//Joodi Al-Asaad
-//Aryan Singh
+// Joodi Al-Asaad
+// Aryan Singh
 
 int main() {
     int counter = 100; 
-    while (true) {
-        std::cout << "Process 2 (exec child): counter = " << counter << ", PID = " << getpid() << std::endl;
+    while (counter > -500) {
+        std::cout << "Process 2 (exec child): counter = " << counter 
+                  << ", PID = " << getpid() << std::endl;
         counter--;      
         sleep(1);       
     }
+
+    std::cout << "Process 2 has reached below -500, terminating now." << std::endl;
     return 0;
 }
