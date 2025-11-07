67a30d3671e3feeffb12b9116ebbb04f9973ea52
Joodi <joodialasaad@cmail.carleton.ca>
2025-11-07 00:51:10 -0500

    implemented two concurrent infinite processes using fork()

--- A2-PART2.cpp
+++ A2-PART2.cpp
@@ -10,21 +10,20 @@ int main() {
         return 1;
     }
     else if (pid == 0) {
-        for (int i = 0; i < 5; ++i) {
-            std::cout << "Child process: iteration " << i 
-                      << ", PID = " << getpid() << std::endl;
+        int counter = 0;
+        while (true) {
+            std::cout << "Process 2 (child): counter = " << counter << ", PID = " << getpid() << std::endl;
+            counter++;
+            sleep(1); 
+        }
+    } 
+    else {
+        int counter = 0;
+        while (true) {
+            std::cout << "Process 1 (parent): counter = " << counter << ", PID = " << getpid() << std::endl;
+            counter++;
             sleep(1);
         }
-        std::cout << "Child process completed." << std::endl;
-    } else {
-        for (int i = 0; i < 5; ++i) {
-            std::cout << "Parent process: iteration " << i 
-                      << ", PID = " << getpid() << std::endl;
-            sleep(1);
-        }
-        wait(nullptr); 
-        std::cout << "Parent: child has finished." << std::endl;
     }
-
     return 0;
 }
