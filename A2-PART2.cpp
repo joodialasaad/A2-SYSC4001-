0435a881ca724a75780b2728581b8650b24020dc
Joodi <joodialasaad@cmail.carleton.ca>
2025-11-07 18:58:14 -0500

    implemented shared memory parent process with counter synchronization and cleanup

--- A2-PART2.cpp
+++ A2-PART2.cpp
@@ -14,21 +14,21 @@ struct SharedData {
 };
 
 int main() {
-    key_t key = ftok("A2-PART2.cpp", 65);  
+    key_t key = ftok("A2-PART2.cpp", 65);
     int shmid = shmget(key, sizeof(SharedData), 0666 | IPC_CREAT);
     if (shmid < 0) {
-        std::cerr << "Shared memory creation failed!" << std::endl;
+        std::cerr << "Parent: Shared memory creation failed!" << std::endl;
         return 1;
     }
 
     SharedData* data = (SharedData*)shmat(shmid, nullptr, 0);
     if (data == (void*)-1) {
-        std::cerr << "Shared memory attachment failed!" << std::endl;
+        std::cerr << "Parent: Shared memory attachment failed!" << std::endl;
         return 1;
     }
 
-    data->multiple = 3;  
-    data->counter = 0;
+    data->multiple = 3;   // the multiple to check
+    data->counter = 0;    // shared counter
 
     pid_t pid = fork();
 
@@ -42,9 +42,11 @@ int main() {
         return 1;
     } 
     else {
-        while (data->counter <= 50) {
+        // Parent process
+        while (data->counter <= 150) {
             if (data->counter % data->multiple == 0)
-                std::cout << "Parent: " << data->counter << " is a multiple of " << data->multiple << std::endl;
+                std::cout << "Parent: " << data->counter << " is a multiple of " 
+                          << data->multiple << std::endl;
             else
                 std::cout << "Parent: counter = " << data->counter << std::endl;
 
@@ -53,7 +55,7 @@ int main() {
         }
 
         wait(nullptr);
-        std::cout << "Parent: counter > 50, cleaning up shared memory." << std::endl;
+        std::cout << "Parent: counter > 150, cleaning up shared memory." << std::endl;
 
         shmdt(data);
         shmctl(shmid, IPC_RMID, nullptr);
